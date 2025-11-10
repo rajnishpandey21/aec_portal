033e88c4164a90edf833e7e8f7139020d5f92e3b
rajnishpandey21 <prajnish482@gmail.com>
2025-11-10 11:56:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. PASTE Your Web App API URL here
+const API_URL = "https://script.google.com/macros/s/AKfycbzYQu2DRXwwLm5Pc06337NHTLVaGJtKANbZ8JfXLgC9tVGxl8TWk35WioHwqX61PU3cKA/exec";
+
+const GOOGLE_CLIENT_ID = "341080038499-8u5pkuog0fa4nma52so00jh8f0oqstpl.apps.googleusercontent.com";
+// 2. FILL IN Your Branch Details here
+// This populates the dropdown menu in your portal.
+const BRANCHES = [
+  { 
+    name: "Branch A", 
+    id: "1bXJ-OuG1pUNIvx4iSf0sV3R8BSwEFHE6m2jtG85r-tY" // Paste Branch A's Sheet ID
+  },
+  { 
+    name: "Delhi", 
+    id: "1NnXtQy0_TZRM4c4FdcDLgdSfvl1ueVk71U5e9Vu2sWg" // Paste Branch B's Sheet ID
+  }
+  // Add more branches here as needed
+];
